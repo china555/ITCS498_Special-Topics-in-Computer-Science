74a9b93461b311267145ec6f581006a320ed2e6e
Chinapanda <kittikornchina2014@gmail.com>
2021-03-22 10:46:25 +0700

    workshop 9 with docker

--- Workshop 9/workshop/model.py
+++ Workshop 9/workshop/model.py
@@ -1,11 +1,10 @@
 import joblib
-
 # Load model
 model_file = 'model.joblib'
 model = joblib.load(model_file)
 
+
 def predict(value):
     # YOUR CODE HERE
-    # pred = ...
-
-    return pred
+    pred = model.predict([value])
+    return pred[0]
